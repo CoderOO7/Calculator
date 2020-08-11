472ae9537bf606aa6be07c798541237448351be9
coderoo7 <shubhamyadav.70007@gmail.com>
2020-08-11 23:48:27 +0530

    Code refactoring using BEM pattern

--- scripts/main.js
+++ scripts/main.js
@@ -1,77 +1,74 @@
 //Global Variables
-const calculate = { num1: '', num2: '', operator: '', result: '' };
+const calculate = { num1: "", num2: "", operator: "", result: "" };
 let isOperatorPressedOnce = false;
 
 //Selecting DOM Nodes
-const buttonContainer = document.getElementById('button-container');
-const mediumDisplay = document.getElementById('medium');
-const smallDisplay = document.getElementById('small');
+const buttonContainer = document.querySelector(".container");
+const resultDisplay = document.querySelector(".display__result");
+const inputDisplay = document.querySelector(".display__input");
 
-buttonContainer.addEventListener('click', activateButtons);
-buttonContainer.addEventListener('transitionend', removeTransition);
+buttonContainer.addEventListener("click", activateButtons);
+buttonContainer.addEventListener("transitionend", removeTransition);
 
 function activateButtons(e) {
-    if(e.target.parentElement.id.includes("button-container"))
-        e.target.classList.add("clicked");
+  if (e.target.parentElement.id.includes("container"))
+    e.target.classList.add("clicked");
 
-    if (e.target.className.includes('misc__btn')) {
-        if (e.target.dataset.operator.includes('result')) {
-            return operate(calculate);
-        }
-        if (e.target.dataset.operator.includes('reset')) {
-            return resetCalculator();
-        }
-        if (e.target.dataset.operator.includes('backspace')) {
-            return clearInput();
-        }
+  if (e.target.className.includes("calc-btn--action")) {
+    if (e.target.dataset.action.includes("result")) {
+      return operate(calculate);
+    }
+    if (e.target.dataset.action.includes("reset")) {
+      return resetCalculator();
+    }
+    if (e.target.dataset.action.includes("undo")) {
+      return clearInput();
+    }
+  }
+
+  if (e.target.className.includes("operator")) {
+    if (!isOperatorPressedOnce) isOperatorPressedOnce = true;
+    else {
+      calculate.num1 = calculate.result;
+      calculate.num2 = "";
     }
 
-    if (e.target.className.includes('operator')) {
-        if (!isOperatorPressedOnce)
-            isOperatorPressedOnce = true;
-        else {
-            calculate.num1 = calculate.result;
-            calculate.num2 = '';
-        }
+    calculate.operator = e.target.dataset.operator;
+    fillinputDisplay(e.target.textContent);
+  }
 
-        calculate.operator = e.target.dataset.operator;
-        fillSmallDisplay(e.target.textContent);
-    }
+  if (e.target.className.includes("number")) {
+    if (isOperatorPressedOnce && calculate.num1 !== "") {
+      calculate.num2 += e.target.textContent;
+      operate(calculate);
+    } else calculate.num1 += e.target.textContent;
 
-    if (e.target.className.includes('number')) {
-        if (isOperatorPressedOnce && calculate.num1 !== '') {
-            calculate.num2 += e.target.textContent;
-            operate(calculate);
-        }
-        else
-            calculate.num1 += e.target.textContent;
-
-        fillSmallDisplay(e.target.textContent);
-    }
+    fillinputDisplay(e.target.textContent);
+  }
 }
 
 function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('clicked');
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("clicked");
 }
 
-const fillSmallDisplay = (string) => {
-    if(getInputTextWidth(smallDisplay) < getContentWidth(smallDisplay))
-        smallDisplay.textContent += string
-    else{
-        let newStr = smallDisplay.textContent+string;
-        smallDisplay.textContent = newStr.slice(1, newStr.length - 1);
-    }
-}
+const fillinputDisplay = string => {
+  if (getInputTextWidth(inputDisplay) < getContentWidth(inputDisplay))
+    inputDisplay.textContent += string;
+  else {
+    let newStr = inputDisplay.textContent + string;
+    inputDisplay.textContent = newStr.slice(1, newStr.length - 1);
+  }
+};
 
-const fillMediumDisplay = (string) => { 
-    if(getInputTextWidth(mediumDisplay) < getContentWidth(mediumDisplay))
-        mediumDisplay.textContent = string;
-    else{
-        let newStr = mediumDisplay.textContent+string;
-        mediumDisplay.textContent = mewStr.slice(1, newStr.length - 1);
-    }
-}
+const fillresultDisplay = string => {
+  if (getInputTextWidth(resultDisplay) < getContentWidth(resultDisplay))
+    resultDisplay.textContent = string;
+  else {
+    let newStr = resultDisplay.textContent + string;
+    resultDisplay.textContent = mewStr.slice(1, newStr.length - 1);
+  }
+};
 
 const add = (num1, num2) => num1 + num2;
 
@@ -79,93 +76,86 @@ const substract = (num1, num2) => num1 - num2;
 
 const multiply = (num1, num2) => num1 * num2;
 
-const divide = (num1, num2) => num2 === 0 ? "GET OUT !!" : num1 / num2;
+const divide = (num1, num2) => (num2 === 0 ? "GET OUT !!" : num1 / num2);
 
-const percentage = (num1, num2) => num2 !== '' ? (num1 / 100) * num2 : num1 / 100;
+const percentage = (num1, num2) =>
+  num2 !== "" ? (num1 / 100) * num2 : num1 / 100;
 
 const operate = ({ num1, num2, operator }) => {
-    let result = null;
-    num1 = Number(num1);
-    num2 = Number(num2);
+  let result = null;
+  num1 = Number(num1);
+  num2 = Number(num2);
 
-    if (operator === "+")
-        result = add(num1, num2);
-    else if (operator === "-")
-        result = substract(num1, num2);
-    else if (operator === "x")
-        result = multiply(num1, num2);
-    else if (operator === "÷")
-        result = divide(num1, num2);
-    else if (operator === "%")
-        result = percentage(num1, num2);
+  if (operator === "+") result = add(num1, num2);
+  else if (operator === "-") result = substract(num1, num2);
+  else if (operator === "x") result = multiply(num1, num2);
+  else if (operator === "÷") result = divide(num1, num2);
+  else if (operator === "%") result = percentage(num1, num2);
 
-    calculate.result = filterResult(result);
-    fillMediumDisplay(calculate.result);
-    console.log({ calculate })
-}
+  calculate.result = filterResult(result);
+  fillresultDisplay(calculate.result);
+  console.log({ calculate });
+};
 
-const filterResult = (result) => {
-    return Number.isNaN(Number(result)) ? result : result % 1 != 0 ? result.toFixed(2) : result.toString();
-}
-
+const filterResult = result => {
+  return Number.isNaN(Number(result))
+    ? result
+    : result % 1 != 0
+    ? result.toFixed(2)
+    : result.toString();
+};
 
 const clearInput = () => {
-    console.log('clearInput');
-    const string = smallDisplay.textContent;
-    smallDisplay.textContent = string.slice(0, string.length - 1);
-}
+  console.log("clearInput");
+  const string = inputDisplay.textContent;
+  inputDisplay.textContent = string.slice(0, string.length - 1);
+};
 
 const resetCalculator = () => {
-    for (const key in calculate) {
-        calculate[key] = '';
-    }
-    isOperatorPressedOnce = false;
-    smallDisplay.textContent = '';
-    mediumDisplay.textContent = '';
-}
+  for (const key in calculate) {
+    calculate[key] = "";
+  }
+  isOperatorPressedOnce = false;
+  inputDisplay.textContent = "";
+  resultDisplay.textContent = "";
+};
 
-const getContentWidth = (element) => {
-    let styles = window.getComputedStyle(element);
-  
-    return element.clientWidth
-      - parseFloat(styles.paddingLeft)
-      - parseFloat(styles.paddingRight)
-}
+const getContentWidth = element => {
+  let styles = window.getComputedStyle(element);
 
-const getInputTextWidth = (element) => {
-    let c = document.createElement("canvas");
-    let ctx = c.getContext("2d");
-    let txtWidth = ctx.measureText(element.textContent).width;
-    return txtWidth;
-}
+  return (
+    element.clientWidth -
+    parseFloat(styles.paddingLeft) -
+    parseFloat(styles.paddingRight)
+  );
+};
 
-document.addEventListener('keydown', e => {
-    console.log(e.key);
-    if (e.key === '.' || e.key >= 0 && e.key <= 9) {
-        document.querySelector(`button[data-number='${e.key}']`).click();
-    }
-    else if (e.key === "+") {
-        document.querySelector(`button[data-operator='${e.key}']`).click();
-    }
-    else if (e.key === "-") {
-        document.querySelector(`button[data-operator='${e.key}']`).click();
-    }
-    else if (e.key === "*") {
-        document.querySelector(`button[data-operator='x']`).click();
-    }
-    else if (e.key === "/") {
-        document.querySelector(`button[data-operator='÷']`).click();
-    }
-    else if (e.key === "%") {
-        document.querySelector(`button[data-operator='${e.key}']`).click();
-    }
-    else if (e.key === "Backspace") {
-        document.querySelector(`button[data-operator='backspace']`).click();
-    }
-    else if (e.key === "Escape" || e.key === "Delete") {
-        document.querySelector(`button[data-operator='reset']`).click();
-    }
-    else if (e.key === "Enter") {
-        document.querySelector(`button[data-operator='=']`).click();
-    }
-})
+const getInputTextWidth = element => {
+  let c = document.createElement("canvas");
+  let ctx = c.getContext("2d");
+  let txtWidth = ctx.measureText(element.textContent).width;
+  return txtWidth;
+};
+
+document.addEventListener("keydown", e => {
+  console.log(e.key);
+  if (e.key === "." || (e.key >= 0 && e.key <= 9)) {
+    document.querySelector(`button[data-number='${e.key}']`).click();
+  } else if (e.key === "+") {
+    document.querySelector(`button[data-operator='${e.key}']`).click();
+  } else if (e.key === "-") {
+    document.querySelector(`button[data-operator='${e.key}']`).click();
+  } else if (e.key === "*") {
+    document.querySelector(`button[data-operator='x']`).click();
+  } else if (e.key === "/") {
+    document.querySelector(`button[data-operator='÷']`).click();
+  } else if (e.key === "%") {
+    document.querySelector(`button[data-operator='${e.key}']`).click();
+  } else if (e.key === "Backspace") {
+    document.querySelector(`button[data-action='undo']`).click();
+  } else if (e.key === "Escape" || e.key === "Delete") {
+    document.querySelector(`button[data-action='reset']`).click();
+  } else if (e.key === "Enter") {
+    document.querySelector(`button[data-operator='=']`).click();
+  }
+});
